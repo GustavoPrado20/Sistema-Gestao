dc7ef6027282c753e1cb240285ec57919a9e6d21
Gustavo Prado <gustavo.henrique.igt@gmail.com>
2024-11-08 17:25:47 -0300

    iniciando pagina dashboar
    iniciando html e css da pagina

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,35 @@
 @extends('layouts.esqueleto')
 
 @section('styles')
-
+    <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
 @endsection
 
 @section('conteudo')
-fnerwfgbhnwerufgbhweiofgopiu
+    <!-- Conteúdo do dashboard aqui -->
+    <main>
+        <header>
+            <section class="titulo">
+                <span><ion-icon name="stats-chart"></ion-icon></span>
+                <h1>DASHBOARD</h1>
+            </section>
+        </header>
+
+        <section class="container-card">
+            <section class="card">
+
+            </section>
+
+            <section class="card">
+                
+            </section>
+
+            <section class="card">
+                
+            </section>
+
+            <section class="card">
+                
+            </section>
+        </section>
+    </main>
 @endsection
